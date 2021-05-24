b6160d9f95827f02c150360d3b5a7dfa659ba962
Saifuddin Saifee <saifuddin.saifee@live.co.uk>
2021-05-24 21:06:31 +0530

    Day 0 - Hello World : Tutorial Completed Challenge Complete

---
+++ Day 0 - Print Hello World/src/com/company/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Write your print statements here!!
+        System.out.println("Hello World.");
+        System.out.println("Welcome to 30 Days of Code.");
+
+    }
+}
