685c6588808d969e3ad8ba0a8bf5d935345b6135
Saifuddin Saifee <saifuddin.saifee@live.co.uk>
2021-05-25 00:59:20 +0530

    Day 1 Class added: Car

---
+++ Day 1 - Data Types! (+ Christmas Trees & Cars)/src/com/company/Car.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Car {
+        int maxSpeed = 100;
+        int minSpeed = 0;
+        double weight = 4732.43;
+        boolean isCarOn = true;
+        char carGrade = 'A';
+
+        public void printVariable(){
+            System.out.println(maxSpeed);
+            System.out.println(minSpeed);
+            System.out.println(weight);
+            System.out.println(isCarOn);
+            System.out.println(carGrade);
+        }
+
+}
